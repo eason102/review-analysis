a8799d62ba625ae73451762172bbe386a6f5b911
yichen_0403 <104032good@gmail.comm>
2022-03-02 16:09:18 +0800

    calt review average

---
+++ read.py
@@ -0,0 +1,12 @@
+data = []
+count = 0
+with open('reviews.txt', 'r') as f:
+    for line in f:
+        data.append(line)
+        count = count + 1
+        if count % 1000 == 0:
+            print(len(data))
+sum_len = 0
+for d in data:
+    sum_len = sum_len + len(d)
+print(sum_len / len(data) )
