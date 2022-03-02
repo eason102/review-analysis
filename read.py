8be22e28d464c1a3dda7c26e7c47c2cf0f1ef1b0
yichen_0403 <104032good@gmail.comm>
2022-03-02 16:34:47 +0800

    add filter function

--- read.py
+++ read.py
@@ -10,3 +10,16 @@ sum_len = 0
 for d in data:
     sum_len = sum_len + len(d)
 print(sum_len / len(data) )
+
+new = []
+for d in data:    #從清單中讀取
+    if len(d) < 100: #求長度小於一百
+        new.append(d) #加到新的清單裡面
+print(len(new)) #印出新清單裡面有幾筆資料
+
+good = []
+for c in data:          #讀取清單
+    if 'good' in c:     #篩選清單裡面有good的字串
+        good.append(c)  #加到good清單裡面
+print(len(good))        #印出good清單裡面有多少項目
+print(good[1])          #印出清單裡第二個項目
